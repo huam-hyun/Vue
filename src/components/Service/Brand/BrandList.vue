d7ae169b863bc5f6c9f8c40c810a1813a55b20ca
SangHyun <duddjshfl@naver.com>
2021-05-20 21:18:41 +0900

    pagination update

--- src/components/Service/Brand/BrandList.vue
+++ src/components/Service/Brand/BrandList.vue
@@ -2,131 +2,204 @@
     <div>
         <b-input class="searchinput" type="text" placeholder="브랜드 검색" v-model="search" ></b-input><b-button variant="primary" @click="searchbrand" class="searchbutton">검색</b-button>       
         <b-tabs class="card" fill>
-            <b-tab title="한식">
-                <div>
-                <b-card class="brand" v-for="item in items(korean)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
-                    <b-card-text>
-                        <p class="name">{{item.brand_name}}</p><br>
-                        업종: {{item.sector}}<br>
-                        평균 매출액: {{item.average_sales}}<br>
-                        매장 수: {{item.num_of_franchise}}<br>
-                        창업비용: {{item.startup_cost}}
-                    </b-card-text>
-                </b-card>
-                </div>
+            <b-tab title="한식" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(korean)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
                 <b-pagination
+                    class="pagination"
                     v-model="currentPage"
                     :total-rows="rows(korean)"
                     :per-page="perPage">
                 </b-pagination>
             </b-tab>
 
-            <b-tab title="중/일식">
-                <b-card class="brand" v-for="(item, index) in chijap" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="중/일식" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(chijap)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(chijap)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
 
-            <b-tab title="분식">
-                <b-card class="brand" v-for="(item, index) in kimbob" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="분식" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(kimbob)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(kimbob)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
 
-            <b-tab title="패스트푸드">
-                <b-card class="brand" v-for="(item, index) in fastfood" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="패스트푸드" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(fastfood)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(fastfood)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
 
-            <b-tab title="치킨">
-                <b-card class="brand" v-for="(item, index) in chicken" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="치킨" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(chicken)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(chicken)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
 
-            <b-tab title="제과제빵">
-                <b-card class="brand" v-for="(item, index) in bread" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="제과제빵" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(bread)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(bread)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
 
-            <b-tab title="카페">
-                <b-card class="brand" v-for="(item, index) in cafe" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="카페" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(cafe)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(cafe)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
 
-            <b-tab title="주점">
-                <b-card class="brand" v-for="(item, index) in hof" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="주점" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(hof)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(hof)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
 
-            <b-tab title="기타외식">
-                <b-card class="brand" v-for="(item, index) in etc" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="기타외식" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(etc)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(etc)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
 
-            <b-tab title="기타외국식">
-                <b-card class="brand" v-for="(item, index) in etc_f" :key="index" @click="detail(item.brand_name)">
-                    <b-card-text>
-                        {{item.brand_name}}<br>
-                        업종: {{item.sector}}<br>
-                        id: {{item.id}}
-                    </b-card-text>
-                    
-
-                </b-card>
+            <b-tab title="기타외국식" @click="()=>{currentPage = 1}">
+                <table>
+                    <b-card class="brand" v-for="item in items(etc_f)" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                        <b-card-text>
+                            <p class="name">{{item.brand_name}}</p><br>
+                            업종: {{item.sector}}<br>
+                            평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                            매장 수: {{item.num_of_franchise}}<br>
+                            창업비용: {{item.startup_cost}}
+                        </b-card-text>
+                    </b-card>
+                </table>
+                <b-pagination
+                    class="pagination"
+                    v-model="currentPage"
+                    :total-rows="rows(etc_f)"
+                    :per-page="perPage">
+                </b-pagination>
             </b-tab>
             
             
@@ -153,7 +226,7 @@ export default {
             etc_f: null,
             search: '',
             perPage: 20,
-            currentPage: 1,
+            currentPage: 1
             
         }
     },
@@ -209,7 +282,7 @@ export default {
 
 <style>
 .brand{
-    min-height: 200px;
+    min-height: 240px;
     width: 300px;
     margin-left: 10px;
     margin-top: 10px;
@@ -228,5 +301,16 @@ export default {
 .name{
     font-weight: 700;
     font-size: 20px;
+}
+label{
+    margin-bottom: 0px;
+}
+.pagination{
+    position: absolute;
+    width: 100px;
+    margin-top: 10px;
+    margin-left: -50px;
+    left: 45%
+
 }
 </style>
