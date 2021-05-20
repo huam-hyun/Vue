f43b1133a14383fec68563f6c87e37f9007f12e4
SangHyun <duddjshfl@naver.com>
2021-05-20 22:12:20 +0900

    BrandDetail update

--- src/components/Service/Brand/BrandDetail.vue
+++ src/components/Service/Brand/BrandDetail.vue
@@ -4,18 +4,41 @@
             <b-card-text>
                 <div>
                     <h1>{{temp}}</h1>
-
+                    <br>
+                    
                     <b-tabs content-class="mt-3">
                         <b-tab title="브랜드 정보" active>
                             <div>
-                                <b-table :items="items"></b-table>
-                                        
+                                <h3>2020년 기준</h3>
+                                <b-table striped hover :items="br_items_1"></b-table>
+                            </div>
+                            <div>
+                                <b-table striped hover :items="br_items_2" ></b-table>
                             </div>
                         </b-tab>
                         <b-tab title="본사 정보">
                             <div>
-                                <b-table :items="items2"></b-table>
+                                <p><strong>본사 기본 정보</strong></p>
+                                <b-table :items="this.hq_basic_items"></b-table>
+                                <b-table :items="this.hq_basic2_items"></b-table>
+                                <hr><br>
+
+                                <p><strong>본사 매출 정보</strong></p>
+                                <b-table :items="this.hq_economic_items"></b-table>
+                                <hr><br>
+
+                                <p><strong>본사 가맹 정보</strong></p>
+                                <b-table :items="this.hq_franchise_items"></b-table>
+                                <hr><br>
+
+                                <p><strong>최근 3년간 법 위반 사실</strong></p>
+                                <b-table :items="this.hq_law_items"></b-table>
+                                <hr><br>
                             </div>
+                            <div>
+                                <p>위 정보는 공정거래위원회에 등록된 가맹사업거래 정보공개서를 분석한 정보입니다. 참고용으로만 활용하시기 바랍니다.</p>
+                            </div>
+
                         </b-tab>
                     </b-tabs>
                 </div>
@@ -30,22 +53,108 @@ import axios from 'axios'
 export default {
     data(){
         return {
-            temp: '브랜드 이름 들어갈 곳',
+            temp: null,
             detail: null,
+            br_detail: null,
+            hq: null,
+            hq_basic_items: null,
+            hq_basic2_items: null,
+            hq_economic_items: null,
+            hq_franchise_items: null,
+            hq_law_items: null,
+            // br_fields_1: ['가맹 시작일', '가맹 개월 수(개월)', '가맹점 수(개)', '임직원 수(명)','가맹 종료(개)','가맹 해지(개)'],
+            // br_fields_2: ['평균매출액(년/천원)', '평당 평균매출액(년/천원)','가맹비(천원)','교육비(천원)','보증금(천원)','기타비용(천원)','창업비용(천원)','면적당 비용(천원)','기준면적(천원)','전체비용(천원)'],
+            br_items_1: new Array,
+            br_items_2: new Array,
+            
         }
     },
     created() {
         axios.get('http://34.64.236.155:8000/myapp/brand/detail/?name=' + this.$route.query.name).then((res) =>{
-            console.log(res)
+            this.br_detail=res.data;
+            console.log(this.br_detail)
             axios.get('http://34.64.236.155:8000/myapp/headquarter/?mutual=' + res.data[0].mutual).then((res) =>{
-                console.log(res)
-                
+                this.hq=res.data[0];
+                console.log('본사' + this.hq)
+                this.temp = this.br_detail[0].brand_name  
+                this.brandGet();
+                this.setHQ();
             })
-            
         })
-        
-        
     },
+
+    methods: {
+      brandGet(){
+        this.br_items_1.push({
+              '가맹 시작일': String(this.br_detail[0].franchise_start_date),
+              '가맹 개월 수(개월)': String(this.br_detail[0].franchise_months).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+              '가맹점 수(개)': String(this.br_detail[0].num_of_franchise).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+              '임직원 수(명)' : String(this.br_detail[0].num_of_employees).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+              '가맹 시작(개)': String(this.br_detail[0].num_of_open).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+              '가맹 종료(개)': String(this.br_detail[0].num_of_quit).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+              '가맹 해지(개)': String(this.br_detail[0].num_of_cancel).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          })
+        this.br_items_2.push({
+            '평균매출액(년/천원)': String(this.br_detail[0].average_sales).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ,
+            '평당 평균매출액(년/천원)': String(this.br_detail[0].average_sales_per_area).replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+            '가맹비(천원)': String(this.br_detail[0].franchise_fee).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ,
+            '교육비(천원)': String(this.br_detail[0].education_fee).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ,
+            '보증금(천원)': String(this.br_detail[0].deposit).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ,
+            '기타비용(천원)': String(this.br_detail[0].other_cost).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ,
+            '창업비용(천원)': String(this.br_detail[0].startup_cost).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ,
+            '면적당 비용(천원)': String(this.br_detail[0].cost_per_area).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ,
+            '기준면적(천원)': String(this.br_detail[0].standard_area).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ,
+            '전체비용(천원)': String(this.br_detail[0].total_cost).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        })
+
+          console.log('br_items_1' + this.br_items_1)
+      },
+      setHQ(){
+            this.hq_basic_items = [
+                {
+                    '상호': this.hq.mutual, '대표자': this.hq.representative, '법인 설립 등기일': this.hq.register_law_date, '사업자 등록일': this.hq.register_biz_date, '대표 번호': this.hq.representative_number, 
+                }
+            ]
+            this.hq_basic2_items = [
+                {
+                    '대표 팩스 번호': this.hq.fax_number, '주소': this.hq.address, '사업자 유형': this.hq.biz_type, '법인 등록 번호': this.hq.law_number, '사업자 등록 번호': this.hq.biz_nubmer,
+                }
+            ]
+            this.hq_economic_items = [
+                {
+                    '연도': '2020년', '자산': this.hq.assets_2020, '부채': this.hq.liabilities_2020, '자본': this.hq.equity_2020, '매출액': this.hq.sales_2020, '영업이익': this.hq.income_2020, '당기순이익': this.hq.net_income_2020
+                }, 
+                {
+                    '연도': '2019년', '자산': this.hq.assets_2019, '부채': this.hq.liabilities_2019, '자본': this.hq.equity_2019, '매출액': this.hq.sales_2019, '영업이익': this.hq.income_2019, '당기순이익': this.hq.net_income_2019
+                },
+                {
+                    '연도': '2018년', '자산': this.hq.assets_2018, '부채': this.hq.liabilities_2018, '자본': this.hq.equity_2018, '매출액': this.hq.sales_2018, '영업이익': this.hq.income_2018, '당기순이익': this.hq.net_income_2018
+                },
+                {
+                    '연도': '2017년', '자산': this.hq.assets_2017, '부채': this.hq.liabilities_2017, '자본': this.hq.equity_2017, '매출액': this.hq.sales_2017, '영업이익': this.hq.income_2017, '당기순이익': this.hq.net_income_2017
+                }
+            ]
+            this.hq_franchise_items = [
+                {
+                    '연도': '2020년', '개점': this.hq.opening_2020, '폐점': this.hq.closing_2020, '명의변경': this.hq.name_change_2020,
+                },
+                {
+                    '연도': '2019년', '개점': this.hq.opening_2019, '폐점': this.hq.closing_2019, '명의변경': this.hq.name_change_2019,
+                },
+                {
+                    '연도': '2018년', '개점': this.hq.opening_2018, '폐점': this.hq.closing_2019, '명의변경': this.hq.name_change_2018,
+                },
+                {
+                    '연도': '2017년', '개점': this.hq.opening_2017, '폐점': this.hq.closing_2017, '명의변경': this.hq.name_change_2017,
+                }                
+            ]
+            this.hq_law_items = [
+                {
+                    '공정거래위원회 시정 조치': this.hq.num_of_correction, '민사소송 패소 및 민사상 화해': this.hq.num_loss_of_law, '형의 선고': this.hq.num_of_sentences
+                }
+            ]
+        },
+    }
 }
 </script>
 
--- src/components/Service/Brand/BrandSearch.vue
+++ src/components/Service/Brand/BrandSearch.vue
@@ -1,8 +1,23 @@
 <template>
     <div>
-        <p>{{this.$route.params.search}}에 대한 검색 결과</p>
-        <p v-if="result == null">검색결과가 없습니다</p>
-        <p v-else>검색결과 나올 부분</p>
+        <p>{{this.$route.query.searchparam}}에 대한 검색 결과</p>
+        <table>
+            <b-card class="brand" v-for="item in items" :key="item.brand_name" @click="detail(item.brand_name)" :per-page="perPage">
+                <b-card-text>
+                    <p class="name">{{item.brand_name}}</p><br>
+                    업종: {{item.sector}}<br>
+                    평균 매출액: <label v-if="item.average_sales == 0">비공개</label><label v-else>{{item.average_sales}}</label><br>
+                    매장 수: {{item.num_of_franchise}}<br>
+                    창업비용: {{item.startup_cost}}
+                </b-card-text>
+            </b-card>
+        </table>
+        <b-pagination
+            class="pagination"
+            v-model="currentPage"
+            :total-rows="rows"
+            :per-page="perPage">
+        </b-pagination>
     </div>
 </template>
 
@@ -11,20 +26,67 @@ import axios from 'axios'
 
 export default {
     name: 'Search',
+    conputed:{
+        rows(){
+            return this.result.length
+        },
+        items(){
+            const items = this.result
+            return items.slice(
+                (this.currentPage -1) * this.perPage,
+                this.currentPage * this.perPage
+            )
+        },
+        
+    },
     data(){
         return {
             result: null,
+            currentPage: 1,
+            perPage: 20,
         }
     },
     created(){
-        var url = 'http://127.0.0.1:8000/myapp/brand/?name=';
+        var url = 'http://34.64.236.155:8000/myapp/brand/?name=';
         axios.get(url + this.$route.query.searchparam).then((res)=>{
              console.log(res);
+             this.result = res.data
         });
-    }
+    },
+    methods:{
+        detail(name){
+            this.$router.push({
+                name: 'BrandDetail',
+                query: {name: name},
+            })
+        },
+    },
+    
 }
 </script>
 
 <style>
-
+.brand{
+    min-height: 240px;
+    width: 300px;
+    margin-left: 10px;
+    margin-top: 10px;
+}
+.pagination{
+    position: absolute;
+    width: 100px;
+    margin-top: 10px;
+    margin-left: -50px;
+    left: 45%
+}
+.name{
+    font-weight: 700;
+    font-size: 20px;
+}
+label{
+    margin-bottom: 0px;
+}
+.card{
+    float:left;
+}
 </style>
